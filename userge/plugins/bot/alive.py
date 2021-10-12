2308c5110274e371a4d39f6da5ec04706529480f
Ryuk <88324835+Anonymousx97@users.noreply.github.com>
2021-10-12 12:31:01 +0530

    Update alive.py

--- userge/plugins/bot/alive.py
+++ userge/plugins/bot/alive.py
@@ -264,11 +264,7 @@ class Bot_Alive:
     @staticmethod
     def alive_default_imgs() -> str:
         alive_imgs = [
-            "https://telegra.ph/file/64e0fa12384e86bc3a764.jpg",
-            "https://i.imgur.com/uzKdTXG.jpg",
-            "https://telegra.ph/file/6ecab390e4974c74c3764.png",
-            "https://telegra.ph/file/995c75983a6c0e4499b55.png",
-            "https://telegra.ph/file/86cc25c78ad667ca5e691.png",
+            "https://telegra.ph/file/64e0fa12384e86bc3a764.jpg"
         ]
         return rand_array(alive_imgs)
 
